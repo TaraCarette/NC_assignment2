4ee539ab2a3b0741c6c7f83fcbb4938fdcddaef9
TaraCarette <terra.the.great@gmail.com>
2022-02-21 14:57:10 +0100

    Create begins of q2

---
+++ q2.py
@@ -0,0 +1,28 @@
+initVel = 10
+initPos = 20
+
+# first settings
+w = 0.5
+alpha1 = 1.5
+alpha2 = 1.5
+r1 = 0.5
+r2 = 0.5
+
+# second settings
+# w = 0.7
+# alpha1 = 1.5
+# alpha2 = 1.5
+# r1 = 1
+# r2 = 1
+
+
+
+# minimization f(x) = x ** 2
+
+def main():
+    print("running")
+
+
+
+if __name__ == '__main__':
+    main()
