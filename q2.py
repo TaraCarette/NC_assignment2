49dc9b8dc8da19a08090bf8d2115f1e131c0942e
TaraCarette <terra.the.great@gmail.com>
2022-02-21 21:11:44 +0100

    Added graphing ability

--- q2.py
+++ q2.py
@@ -1,38 +1,41 @@
-initVel = 10
+import matplotlib.pyplot as plt
+
+initVel = 10 # pointing away from the optimum
 initPos = 20
 
 termLoops = 50
 
 # first settings
-w = 0.5
-alpha1 = 1.5
-alpha2 = 1.5
-r1 = 0.5
-r2 = 0.5
-
-# second settings
-# w = 0.7
+# w = 0.5
 # alpha1 = 1.5
 # alpha2 = 1.5
-# r1 = 1
-# r2 = 1
+# r1 = 0.5
+# r2 = 0.5
+
+# second settings
+w = 0.7
+alpha1 = 1.5
+alpha2 = 1.5
+r1 = 1
+r2 = 1
 
 
 def fitness(x):
     return x ** 2
 
-def main():
-    print("running")
 
+# need to make position a vector
+def main():
     velocity = initVel
     position = initPos
     lBest = position
     gBest = position
 
+    pList = [position]
+
     counter = 0
     while counter < termLoops:
         counter += 1
-        print(position)
 
         # for each particle, which in this case is just one
         # do not need to create random vectors here
@@ -48,11 +51,20 @@ def main():
             lBest = position
 
         # update global best
-        # as there is only a single particle the best is simply itself
         if fitness(position) < fitness(gBest):
             gBest = position
+
+        pList.append(position)
+
+    return pList
 
 
 
 if __name__ == '__main__':
-    main()
+    positions = main()
+
+    plt.plot(list(range(0, termLoops + 1)), positions)
+    plt.xlabel('Iteration')
+    plt.ylabel('Current Position')
+    plt.title('Particle Position Over Time')
+    plt.show()
