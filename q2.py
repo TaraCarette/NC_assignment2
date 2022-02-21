ebd69d6cdbee8445d96ee85bb3f91ff11f320f23
TaraCarette <terra.the.great@gmail.com>
2022-02-21 17:52:46 +0100

    Tentatively working. Slightly unexpected results so may need tweaking. No graphs yet

--- q2.py
+++ q2.py
@@ -1,6 +1,8 @@
 initVel = 10
 initPos = 20
 
+termLoops = 50
+
 # first settings
 w = 0.5
 alpha1 = 1.5
@@ -16,11 +18,39 @@ r2 = 0.5
 # r2 = 1
 
 
-
-# minimization f(x) = x ** 2
+def fitness(x):
+    return x ** 2
 
 def main():
     print("running")
+
+    velocity = initVel
+    position = initPos
+    lBest = position
+    gBest = position
+
+    counter = 0
+    while counter < termLoops:
+        counter += 1
+        print(position)
+
+        # for each particle, which in this case is just one
+        # do not need to create random vectors here
+
+        # update velocity
+        velocity = (w * velocity) + (alpha1 * r1 * (lBest - position)) + (alpha2 * r2 * (gBest - position))
+
+        # update positions
+        position = position + velocity
+
+        # update local best
+        if fitness(position) < fitness(lBest):
+            lBest = position
+
+        # update global best
+        # as there is only a single particle the best is simply itself
+        if fitness(position) < fitness(gBest):
+            gBest = position
 
 
 
